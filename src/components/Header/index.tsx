a86680862902fe14833b1a4c293cee5909c4a6eb
emerson vieira <developer.emersonjr@gmail.com>
2022-04-12 20:55:00 -0300

    initil commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { SignInButton } from "../SignInButton";
+
+import styles from "./styles.module.scss"
+
+export function Header(){
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news" />
+                <nav>
+                    <a href="" className={styles.active}>home</a>
+                    <a href="">Posts</a>
+                </nav>
+                <SignInButton />
+            </div>
+        </header>
+    );
+}
