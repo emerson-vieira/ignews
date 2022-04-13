594e50d9e82d6a80eb87d1b05efb8b1cc7e8e104
emerson vieira <developer.emersonjr@gmail.com>
2022-04-13 01:00:33 -0300

    fauna db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query } from "faunadb";
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../service/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -14,4 +18,38 @@ export default NextAuth({
         }
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index("user_by_email"),
+                  query.Casefold(user.email)
+                )
+              )
+            ),
+            query.Create(
+              query.Collection("users"),
+              { data: { email } } 
+            ),
+            query.Get(
+              query.Match(
+                query.Index("user_by_email"),
+                query.Casefold(user.email)
+              )
+            )
+          )
+        );
+        return true;
+      } catch (error) {
+        return false;
+        
+      }
+    },
+  }
 })
