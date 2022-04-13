790e1992bb65a17f06f64373e63a2d08ed0017ac
emerson vieira <developer.emersonjr@gmail.com>
2022-04-13 00:05:22 -0300

    next-auth

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { SignInButton } from "../SignInButton";
 
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
 
 export function Header(){
     return (
--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,28 @@
+import { signIn, signOut, useSession } from "next-auth/react"
+
 import { FaGithub} from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-    const isUserLoggedIn = false;
-    return isUserLoggedIn ? (
+    const { data: session } = useSession();
+
+    return session ? (
         <button 
-        type="button"
-        className={styles.signInButton}
+            type="button"
+            className={styles.signInButton}
+            onClick={() => signOut()}
         >
             <FaGithub color="#04b361" />
-            Emerson Vieira   
+            {session.user.name}   
             <FiX  color="#737380" className={styles.closeIcon}/>
         </button>   
     ) :(
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417" />
             Sign i with Github    
